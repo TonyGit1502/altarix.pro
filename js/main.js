52c0f49e7417f495747dc339251fe976937530b0
Anton <anton.zyblev.96@mail.ru>
2017-10-12 22:32:59 +0400

    step-5

--- js/main.js
+++ js/main.js
@@ -1,50 +1,54 @@
-'use strict';
-
-var newsApp = angular.module('newsApp', []);
+/* eslint-disable indent */
+const newsApp = angular.module("newsApp", []);
 const mask = "lsNews_";
 
-newsApp.controller('NewsListCtrl', function($scope, $http){
-	$scope.newsList = [];
-	var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=316d3b9e0a304077840e29d0cdd834bc&q=Samara";
-    $http({
-        method : "GET",
-        url : url
-    }).then(function mySuccess(response) {
-        $scope.newsList = response.data.response.docs;
-        console.log($scope.newsList);
-    }, function myError(response) {
-        $scope.newsList = response.statusText;
-    });
-    $scope.saveData = function(e){
-    	var index = localStorage.length;
-		var obj = {
+newsApp.controller("NewsListCtrl", ($scope, $http) => {
+	$scope.searchData = function () {
+  $scope.newsList = [];
+  let url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=316d3b9e0a304077840e29d0cdd834bc&";
+  let query = document.getElementById("query").value;
+  console.log(query);
+  alert(query);
+  url += "q=" + query;
+  $http({
+      method: "GET",
+      url
+  }).then((response) => {
+      $scope.newsList = response.data.response.docs;
+      console.log($scope.newsList);
+  }, (response) => {
+      $scope.newsList = response.statusText;
+  });
+	};
+	$scope.saveData = function (e) {
+		let index = localStorage.length;
+		const obj = {
 			date: e.$$watchers[0].last,
 			news: e.$$watchers[1].last,
-			title: e.$$watchers[2].last 
-		};
-		var serialNews = JSON.stringify(obj);
+			title: e.$$watchers[2].last};
+		const serialNews = JSON.stringify(obj);
 		localStorage.setItem(mask+index, serialNews);
-		index++;	
-    };
+		index++;
+	};
 });
 
-newsApp.controller('WeatherCtrl', function($scope, $http){
+newsApp.controller("WeatherCtrl", ($scope, $http) => {
 	$scope.weather = [];
-	var url = "http://api.openweathermap.org/data/2.5/weather?q=Samara&units=metric&appid=0a9eac8b755627181a3fc1f489d72309";
+	const url = "http://api.openweathermap.org/data/2.5/weather?q=Samara&units=metric&appid=0a9eac8b755627181a3fc1f489d72309";
 	$http({
 		method: "GET",
-		url: url
-	}).then(function success(response){
+		url
+	}).then((response) => {
 		$scope.weather = response.data;
 		console.log($scope.weather);
-	}, function error(response){
+	}, (response) => {
 		$scope.weather = response.statusText;
 	});
 });
 
-newsApp.controller("OutputDataCtrl", function($scope){
+newsApp.controller("OutputDataCtrl", ($scope) => {
 	$scope.returnData = [];
-	for(var i=0; i<localStorage.length; i++){
+	for (let i=0; i<localStorage.length; i++) {
 		$scope.returnData[i] = JSON.parse(localStorage.getItem(mask+i));
 	}
 });
